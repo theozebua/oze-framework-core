3a9d9435cf86abeb9eaaa2861758c39895dd8b54
theozebua <ronizebua.rz@gmail.com>
2022-11-19 18:33:07 +0700

    Create ControllerNotFoundException

---
+++ src/Exceptions/Controller/ControllerNotFoundException.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Theozebua\TeecoderFramework\Exceptions\Controller;
+
+use Exception;
+use Theozebua\TeecoderFramework\Exceptions\BaseException;
+
+class ControllerNotFoundException extends Exception implements BaseException
+{
+    // 
+}
