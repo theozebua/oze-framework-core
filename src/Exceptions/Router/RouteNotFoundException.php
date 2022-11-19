f4d1f8013503b3af436906dd413e3978b8e753f1
theozebua <ronizebua.rz@gmail.com>
2022-11-19 13:44:04 +0700

    Create RouteNotFoundException

---
+++ src/Exceptions/Router/RouteNotFoundException.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Theozebua\TeecoderFramework\Exceptions\Router;
+
+use Exception;
+use Theozebua\TeecoderFramework\Exceptions\BaseException;
+
+class RouteNotFoundException extends Exception implements BaseException
+{
+    // 
+}
