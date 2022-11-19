c791a177db60ee064b550ba9ebb1a8207c153f20
theozebua <ronizebua.rz@gmail.com>
2022-11-19 18:35:10 +0700

    Make route resolver can resolve an array of controller and method

--- src/Interfaces/Router/RouteRegistrarInterface.php
+++ src/Interfaces/Router/RouteRegistrarInterface.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Theozebua\TeecoderFramework\Interfaces\Router;
 
 use Closure;
+use InvalidArgumentException;
 use Theozebua\TeecoderFramework\Exceptions\Router\RouteNotFoundException;
 
 interface RouteRegistrarInterface
@@ -14,20 +15,22 @@ interface RouteRegistrarInterface
      * 
      * @param string $requestMethod
      * @param string $uri
-     * @param Closure $action
+     * @param Closure|array<string, string> $action
      * 
      * @return self
      */
-    public function register(string $requestMethod, string $uri, Closure $action): self;
+    public function register(string $requestMethod, string $uri, Closure|array $action): self;
 
     /**
      * Resolve the registered routes.
      * 
      * @param string $requestUri
+     * @param string $requestMethod
      * 
      * @throws RouteNotFoundException
+     * @throws InvalidArgumentException
      * 
      * @return mixed
      */
-    public function resolve(string $requestUri): mixed;
+    public function resolve(string $requestUri, string $requestMethod): mixed;
 }
--- src/Router/RouteRegistrar.php
+++ src/Router/RouteRegistrar.php
@@ -4,7 +4,10 @@ declare(strict_types=1);
 
 namespace Theozebua\TeecoderFramework\Router;
 
+use BadMethodCallException;
 use Closure;
+use InvalidArgumentException;
+use Theozebua\TeecoderFramework\Exceptions\Controller\ControllerNotFoundException;
 use Theozebua\TeecoderFramework\Exceptions\Router\RouteNotFoundException;
 use Theozebua\TeecoderFramework\Interfaces\Router\RouteRegistrarInterface;
 
@@ -13,14 +16,14 @@ final class RouteRegistrar implements RouteRegistrarInterface
     /**
      * Registered routes.
      * 
-     * @var array<string, array<string, Closure>> $routes
+     * @var array<string, array<string, Closure|array<string, string>>> $routes
      */
     private array $routes = [];
 
     /**
      * {@inheritdoc}
      */
-    final public function register(string $requestMethod, string $uri, Closure $action): self
+    final public function register(string $requestMethod, string $uri, Closure|array $action): self
     {
         $this->routes[$requestMethod][$uri] = $action;
 
@@ -30,18 +33,70 @@ final class RouteRegistrar implements RouteRegistrarInterface
     /**
      * {@inheritdoc}
      */
-    final public function resolve(string $requestUri): mixed
+    final public function resolve(string $requestUri, string $requestMethod): mixed
     {
         $route  = parse_url($requestUri)['path'];
-        $action = $this->routes['GET'][$route] ?? null;
+        $action = $this->routes[$requestMethod][$route] ?? null;
 
         $this->ignoreRoute($route, '/favicon.ico');
 
         if (!$action) {
-            throw new RouteNotFoundException("Route {$route} is not found.");
+            throw new RouteNotFoundException("Route {$route} is not found");
         }
 
-        return call_user_func($action);
+        if (!$action instanceof Closure && !is_array($action)) {
+            throw new InvalidArgumentException("Action must be an instance of \\Closure or array of controller and method");
+        }
+
+        if ($action instanceof Closure) {
+            return call_user_func($action);
+        }
+
+        if (is_array($action)) {
+            return $this->resolveController($action);
+        }
+    }
+
+    /**
+     * Create an instance of a controller class.
+     * 
+     * @param array<string, string> $action
+     * 
+     * @throws ControllerNotFoundException
+     * 
+     * @return mixed
+     */
+    private function resolveController(array $action): mixed
+    {
+        [$class, $method] = $action;
+
+        if (!class_exists($class)) {
+            throw new ControllerNotFoundException("{$class} is not found");
+        }
+
+        $object = new $class();
+
+        return $this->resolveMethod($object, $class, $method);
+    }
+
+    /**
+     * Call a controller's method.
+     * 
+     * @param object $object
+     * @param string $class
+     * @param string $method
+     * 
+     * @throws BadMethodCallException
+     * 
+     * @return mixed
+     */
+    private function resolveMethod(object $object, string $class, string $method): mixed
+    {
+        if (!method_exists($class, $method)) {
+            throw new BadMethodCallException("Method {$method} is not found in {$class}");
+        }
+
+        return call_user_func_array([$object, $method], []);
     }
 
     /**
