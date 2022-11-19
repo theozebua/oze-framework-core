d9ca52a62264ff35ece626604aca07405b842083
theozebua <ronizebua.rz@gmail.com>
2022-11-19 13:42:45 +0700

    Create the base exception interface

---
+++ src/Exceptions/BaseException.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Theozebua\TeecoderFramework\Exceptions;
+
+use Throwable;
+
+/**
+ * This is the base exception interface of Teecoder Framework.
+ */
+interface BaseException extends Throwable
+{
+    // 
+}
