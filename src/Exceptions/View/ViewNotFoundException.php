b44965eb11916c5e03e425579c9f6114660bf718
theozebua <ronizebua.rz@gmail.com>
2022-11-24 19:19:24 +0700

    Create ViewNotFoundException

---
+++ src/Exceptions/View/ViewNotFoundException.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Theozebua\OzeFramework\Exceptions\View;
+
+use Exception;
+use Theozebua\OzeFramework\Exceptions\BaseException;
+
+final class ViewNotFoundException extends Exception implements BaseException
+{
+    // 
+}
