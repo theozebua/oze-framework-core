a8bc11b317010bc14c50d958677ad1f5172010bc
theozebua <ronizebua.rz@gmail.com>
2022-12-17 16:04:59 +0700

    Use query builder

--- src/OzeFramework/Database/Model.php
+++ src/OzeFramework/Database/Model.php
@@ -5,14 +5,24 @@ declare(strict_types=1);
 namespace OzeFramework\Database;
 
 use OzeFramework\App\App;
+use OzeFramework\Database\Query\Builder;
 use OzeFramework\Exceptions\Database\ConfigNotFoundException;
 use OzeFramework\Interfaces\Database\ModelInterface;
 use OzeFramework\Http\Response;
 
 /**
- * @method array all(array $columns = ['*']) Get all data.
- * @method array|false getWhere(string $key, string $operator = '=', mixed $value, array $columns = ['*']) Get data based on given condition.
- * @method object|false firstWhere(string $key, string $operator = '=', mixed $value, array $columns = ['*']) Get first data based on given condition.
+ * @method Builder select(array|string $columns = ['*']) Add SELECT statement to the sql query.
+ * @method Builder where(string $key, mixed $value, string $operator = '=', string $boolean = 'AND', bool $useNot = false) Add WHERE clause to the sql query.
+ * @method Builder whereNot(string $key, mixed $value, string $operator = '=') Add WHERE NOT clause to the sql query.
+ * @method Builder whereLike(string $key, mixed $value) Add WHERE LIKE clause to the sql query.
+ * @method Builder whereNotLike(string $key, mixed $value) Add WHERE NOT LIKE clause to the sql query.
+ * @method Builder orWhere(string $key, mixed $value, string $operator = '=') Add OR WHERE clause to the sql query.
+ * @method Builder orWhereNot(string $key, mixed $value, string $operator = '=') Add OR WHERE NOT clause to the sql query.
+ * @method Builder orWhereLike(string $key, mixed $value) Add OR WHERE LIKE clause to the sql query.
+ * @method Builder orWhereNotLike(string $key, mixed $value) Add OR WHERE NOT LIKE clause to the sql query.
+ * @method Builder limit(int $length, int $offset = 0) Add LIMIT clause to the sql query.
+ * @method array get() Get all data.
+ * @method object first() Get the first data.
  */
 abstract class Model implements ModelInterface
 {
@@ -24,25 +34,11 @@ abstract class Model implements ModelInterface
     private Response $response;
 
     /**
-     * Model name.
-     * 
-     * @var string $model
-     */
-    protected string $model;
-
-    /**
      * Table name.
      * 
      * @var string $table
      */
     protected string $table;
-
-    /**
-     * Builtin Query.
-     * 
-     * @var BuiltinQuery $builtinQuery
-     */
-    private BuiltinQuery $builtinQuery;
 
     /**
      * Model constructor.
@@ -62,17 +58,32 @@ abstract class Model implements ModelInterface
         }
 
         $config             = require $dbConfig;
-        $dbh                = (new Connection($config))->connect();
-        $this->builtinQuery = new BuiltinQuery($dbh, $this->model, $this->table);
+        $this->builder      = new Builder((new Connection($config))->connect(), $this::class, $this->table);
     }
 
+    /**
+     * Set dynamic property.
+     * 
+     * @param string $name
+     * @param mixed $value
+     * 
+     * @return void
+     */
     final public function __set(string $name, mixed $value)
     {
         $this->{$name} = $value;
     }
 
+    /**
+     * Call the query builder method.
+     * 
+     * @param string $name
+     * @param mixed $arguments
+     * 
+     * @return mixed
+     */
     final public function __call(string $name, mixed $arguments): mixed
     {
-        return call_user_func_array([$this->builtinQuery, $name], $arguments);
+        return call_user_func_array([$this->builder, $name], $arguments);
     }
 }
