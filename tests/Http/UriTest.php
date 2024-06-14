cef748eec8e26ac127133d0f0800534429f4a4bc
Theo Zebua <ronizebua.rz@gmail.com>
2024-06-14 09:56:19 +0700

    wip

--- tests/Http/UriTest.php
+++ tests/Http/UriTest.php
@@ -4,20 +4,63 @@ declare(strict_types=1);
 
 namespace OzeFramework\Tests\Http;
 
-use InvalidArgumentException;
 use OzeFramework\Http\Factory\Uri;
-use PHPUnit\Framework\TestCase;
 
-final class ContainerTest extends TestCase
+final class UriTest extends AbstractUri
 {
-    protected string $url = 'http://username:password@hostname:9090/path?arg=value#anchor';
-
-    public function testUriFactoryThrowsInvalidArgumentExceptionIfGivenUriIsInvalid(): void
+    public function testUriReturnsSchema(): void
     {
-        $this->expectException(InvalidArgumentException::class);
+        $scheme = Uri::create($this->url)->getScheme();
 
-        Uri::create('http:///invalid.com');
+        $this->assertSame('http', $scheme);
     }
 
-    // TODO: Add more tests
+    public function testUriReturnsAuthority(): void
+    {
+        $authority = Uri::create($this->url)->getAuthority();
+
+        $this->assertSame('username:password@hostname:9090', $authority);
+    }
+
+    public function testUriReturnsUserInfo(): void
+    {
+        $userInfo = Uri::create($this->url)->getUserInfo();
+
+        $this->assertSame('username:password', $userInfo);
+    }
+
+    public function testUriReturnsHost(): void
+    {
+        $host = Uri::create($this->url)->getHost();
+
+        $this->assertSame('hostname', $host);
+    }
+
+    public function testUriReturnsPort(): void
+    {
+        $port = Uri::create($this->url)->getPort();
+
+        $this->assertSame(9090, $port);
+    }
+
+    public function testUriReturnsPath(): void
+    {
+        $path = Uri::create($this->url)->getPath();
+
+        $this->assertSame('/path', $path);
+    }
+
+    public function testUriReturnsQuery(): void
+    {
+        $query = Uri::create($this->url)->getQuery();
+
+        $this->assertSame('arg=value', $query);
+    }
+
+    public function testUriReturnsFragment(): void
+    {
+        $fragment = Uri::create($this->url)->getFragment();
+
+        $this->assertSame('anchor', $fragment);
+    }
 }
